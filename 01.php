fb9cfe2968950f98e5eed1c1c7ca84fc18bed2a3
EmoLordofDarkness <39245857+EmoLordofDarkness@users.noreply.github.com>
2018-10-02 15:00:56 +0200

    Add files via upload

--- 01.php
+++ 01.php
@@ -1,3 +1,20 @@
 <?php
  echo "hello world" ;
+ 
+ $cislo="8";
+ echo $cislo
+ echo"<br>";
+ 
+ 
+ if ($cislo < 10)
+
+{
+echo 'menší než 10'
+ 
+} 
+ else
+ {
+  $cislo > 10 ;
+echo 'větší než 10'
+ }
 ?>
